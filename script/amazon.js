f7e81eb53157740c82493f5e510bf79288489583
Tracerr <jonathan.alvin2019@gmail.com>
2024-06-15 14:40:23 +0700

    Update html

--- script/amazon.js
+++ script/amazon.js
@@ -22,13 +22,28 @@ const products = [{
     count: 46,
   },
   priceCent: 799, //cents
+}, {
+  image: 'images/products/black-2-slot-toaster.jpg',
+  name: '2 Slot Toaster - Black',
+  rating: {
+    stars: 5,
+    count: 2197,
+  },
+  priceCent: 1899, //cents
+}, {
+  image: 'images/products/6-piece-non-stick-baking-set.webp',
+  name: '6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set',
+  rating: {
+    stars: 4.5,
+    count: 982,
+  },
+  priceCent: 3499, //cents
 }];
 
 let productsHTML = '';
 
 products.forEach((product) => {
   productsHTML += `
-    <div class="products-grid">
       <div class="product-container">
         <div class="product-image-container">
           <img class="product-image"
@@ -48,7 +63,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-price">
-          $${product.priceCent / 100}
+          $${(product.priceCent / 100).toFixed(2)}
         </div>
 
         <div class="product-quantity-container">
@@ -77,106 +92,8 @@ products.forEach((product) => {
           Add to Cart
         </button>
       </div>
-
-      <div class="product-container">
-        <div class="product-image-container">
-          <img class="product-image"
-            src="images/products/intermediate-composite-basketball.jpg">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
-          Intermediate Size Basketball
-        </div>
-
-        <div class="product-rating-container">
-          <img class="product-rating-stars"
-            src="images/ratings/rating-40.png">
-          <div class="product-rating-count link-primary">
-            127
-          </div>
-        </div>
-
-        <div class="product-price">
-          $20.95
-        </div>
-
-        <div class="product-quantity-container">
-          <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-
-        <div class="product-spacer"></div>
-
-        <div class="added-to-cart">
-          <img src="images/icons/checkmark.png">
-          Added
-        </div>
-
-        <button class="add-to-cart-button button-primary">
-          Add to Cart
-        </button>
-      </div>
-
-      <div class="product-container">
-        <div class="product-image-container">
-          <img class="product-image"
-            src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
-        </div>
-
-        <div class="product-name limit-text-to-2-lines">
-          Adults Plain Cotton T-Shirt - 2 Pack
-        </div>
-
-        <div class="product-rating-container">
-          <img class="product-rating-stars"
-            src="images/ratings/rating-45.png">
-          <div class="product-rating-count link-primary">
-            56
-          </div>
-        </div>
-
-        <div class="product-price">
-          $7.99
-        </div>
-
-        <div class="product-quantity-container">
-          <select>
-            <option selected value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-          </select>
-        </div>
-
-        <div class="product-spacer"></div>
-
-        <div class="added-to-cart">
-          <img src="images/icons/checkmark.png">
-          Added
-        </div>
-
-        <button class="add-to-cart-button button-primary">
-          Add to Cart
-        </button>
-      </div>
-    </div>
   `
+  console.log(productsHTML)
 });
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.getElementById('productGrid').innerHTML = productsHTML;
