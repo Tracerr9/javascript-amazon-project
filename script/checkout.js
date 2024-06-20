dda605c38273fe454a2aea0c44291d54005747df
Tracerr <jonathan.alvin2019@gmail.com>
2024-06-20 16:08:08 +0700

    add backend-practice file

--- script/checkout.js
+++ script/checkout.js
@@ -2,6 +2,7 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/cart-Class.js' ;
+import  '../data/backedn-practice.js';
 
 renderOrderSummary();
 renderPaymentSummary();
